a11247ffa9a0bec4f0d8ea8e9d6731289fda3a9f
KhansaMahira <khansamahira011@gmail.com>
2023-09-13 14:40:37 +0700

    tutorial 2 selesai

---
+++ main/forms.py
@@ -0,0 +1,8 @@
+from itertools import product
+from django.forms import ModelForm
+from main.models import Product
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ["name", "price", "description"]
